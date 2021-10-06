7eef6cca855ddc1fd47a66b4b1ce28e5967512a9
EvanLyu732 <zhengshushu1997@gmail.com>
2021-10-06 16:35:00 +0800

    add

---
+++ src/stream_buf.cpp
@@ -0,0 +1,16 @@
+#include <boost/asio.hpp>
+#include <iostream>
+
+using namespace boost;
+
+int main()
+{
+    asio::streambuf buf;
+    std::ostream output(&buf);
+    output << "Messages1\nMessage2";
+
+    std::istream input(&buf);
+    std::string message1;
+    std::getline(input, message1);
+    return 0;
+}
